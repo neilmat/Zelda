f642ed16e91638deec077f304ca315ed05c062a5
neilmat <neilmat@umich.edu>
2014-01-27 13:58:06 -0500

    commit 10
    created basic spawn point

---
+++ Assets/spawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnScript : MonoBehaviour {
+	public Transform effectPrefab;
+	public Transform spawnPrefab;
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		var effect = Instantiate (effectPrefab)as Transform;
+		effect.position = transform.position;
+		Destroy (effect.gameObject, .75f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer > .75)
+		{
+			var spawn = Instantiate(spawnPrefab) as Transform;
+			spawn.position = transform.position;
+			Destroy (this.gameObject);
+		}
+	}
+}
