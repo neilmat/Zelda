2a68213f2d628660bd83ba231b0aa1a373856c30
neilmat <neilmat@umich.edu>
2014-01-27 20:03:40 -0500

    commit 11
    Changed how camera panning works, pans correctly now.
    Changed some collision layer settings

---
+++ Assets/PanScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanScript : MonoBehaviour {
+
+	// Use this for initialization
+	private float panSpeed = .05f;
+	private bool panning;
+	public Vector3 panVector = Vector3.zero;
+	void Start () {
+		panning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(panning)
+		{
+			Camera.main.transform.Translate(panVector);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) 
+	{
+		if(!coll.gameObject.CompareTag("Player")) return;
+		if(!panning)
+		{
+			panVector = Vector3.zero;
+			playerState.disabled = true;
+			panning = true;
+			if(playerState.facing == 0) panVector.y = -panSpeed;
+			else if(playerState.facing == 1) panVector.x = -panSpeed;
+			else if(playerState.facing == 2) panVector.y = panSpeed;
+			else if(playerState.facing == 3) panVector.x = panSpeed;
+		}
+		else
+		{
+			panning = false;
+			playerState.disabled = false;
+		}
+	}
+}
--- Assets/playerState.cs
+++ Assets/playerState.cs
@@ -9,7 +9,5 @@ public class playerState : MonoBehaviour {
 	public static bool attacking = false;
 
 	void Update(){
-		if(edgeDetection.panning) this.gameObject.collider2D.isTrigger = true;
-		else this.gameObject.collider2D.isTrigger = false;
 	}
 }
