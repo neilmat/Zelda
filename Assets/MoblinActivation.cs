1284531c6cadcee7c383ec32f3cf2499207ac18f
neilmat <neilmat@umich.edu>
2014-02-05 20:46:41 -0500

    Moblins
    Created Prefabs for red and blue moblins and an arrow for them to shoot

---
+++ Assets/MoblinActivation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoblinActivation : Activation {
+	public override void activate()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+		gameObject.GetComponent<Animator> ().enabled = true;
+		gameObject.GetComponent<enemyMovement> ().enabled = true;
+		gameObject.GetComponent<MoblinState> ().enabled = true;
+	}
+	
+	public override void deactivate()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		gameObject.GetComponent<Animator> ().enabled = false;
+		gameObject.GetComponent<enemyMovement> ().enabled = false;
+		gameObject.GetComponent<MoblinState> ().enabled = false;
+		gameObject.rigidbody2D.velocity = Vector2.zero;
+	}
+}
