836f797ea8bc8c4304c2c1fa4aedf5e077723cfd
neilmat <neilmat@umich.edu>
2014-02-05 21:43:35 -0500

    Tektites
    Created prefabs for red and blue tektites

---
+++ Assets/TektiteActivation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TektiteActivation : Activation {
+	public override void activate()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+		gameObject.GetComponent<Animator> ().enabled = true;
+		gameObject.GetComponent<TektiteState> ().enabled = true;
+	}
+	
+	public override void deactivate()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		gameObject.GetComponent<Animator> ().enabled = false;
+		gameObject.GetComponent<TektiteState> ().enabled = false;
+		gameObject.rigidbody2D.velocity = Vector2.zero;
+	}
+}
---
+++ Assets/TektiteState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TektiteState : MonoBehaviour {
+	private float timer;
+	private Vector2 vel;
+	public bool jumping = false;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer > 5 && !jumping)
+		{
+			vel.x = Random.Range(-5,6);
+			vel.y = Random.Range(3,9);
+			this.gameObject.GetComponent<Rigidbody2D>().velocity = vel;
+			this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+			timer = 0;
+			jumping = true;
+			this.gameObject.GetComponent<Animator>().SetBool("Jumping", true);
+		}
+		else if(timer > 1 && jumping)
+		{
+			vel.x = vel.y = 0;
+			this.gameObject.GetComponent<Rigidbody2D>().velocity = vel;
+			this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+			timer = 0;
+			jumping = false;
+			this.gameObject.GetComponent<Animator>().SetBool("Jumping", false);
+		}
+	}
+}
+
